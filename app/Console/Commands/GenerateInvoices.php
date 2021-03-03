f86941df6bcc8d0060fdcaf38bb5a49c28c6f6f4
Pranto <prantofrombd@gmail.com>
2021-03-03 11:49:28 +0600

    fix invoice cancelletion

--- app/Console/Commands/GenerateInvoices.php
+++ app/Console/Commands/GenerateInvoices.php
@@ -47,7 +47,7 @@ class GenerateInvoices extends Command
             $customers_wmobile = $customers->where('mobile', 'not like', "%1998811%");
 
             foreach ($customers->get() as $customer) {
-                foreach ($customer->invoices()->where('status', '!=', Invoice::STATUS_PAID)->get() as $invoice) {
+                foreach ($customer->invoices()->get() as $invoice) {
                     $invoice->status = Invoice::STATUS_CANCELLED;
                     $invoice->save();
                     print $invoice->id . PHP_EOL;
