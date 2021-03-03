869c2ad48609a788771d19c75fd7a85d9ce86cac
Pranto <prantofrombd@gmail.com>
2021-03-03 11:19:43 +0600

    generate invoces

---
+++ app/Console/Commands/GenerateInvoices.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class GenerateInvoices extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'invoice:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate invoice for all customers';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // Generate invoice for Somor only
+        $user = User::where(['email' => 'somor@softmight.com'])->first();
+
+        if ($user) {
+            $customers = $user->customers()->where('mobile', 'not like', "%1998811");
+
+            print $customers->count();
+        }
+    }
+}
